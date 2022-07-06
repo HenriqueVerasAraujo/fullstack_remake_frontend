67a477b2e2ad56ec890f4cfd280f3a68e3002af9
Henrique_Veras <theveras1@gmail.com>
2022-07-06 00:10:44 -0300

    setup das rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-5xl text-blue-600">teste</h1>
-    </div>
+ <BrowserRouter>
+  <Routes>
+    <Route exact path="/" element={ <Home /> } />
+  </Routes>
+ </BrowserRouter>
   );
 }
 
